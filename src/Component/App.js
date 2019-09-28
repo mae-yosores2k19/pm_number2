fd5830cb69e321d4cde79fd0681bfcdd762c49d3
mae-yosores2k19 <jessamaehortadoyosores@gmail.com>
2019-09-28 16:12:00 +0800

    Initial Commit

---
+++ src/Component/App.js
@@ -0,0 +1,15 @@
+
+import React,{Component}from 'react';
+import SimpleCalculator from './SimpleCalculator'
+
+
+class App extends Component{
+    render(){
+        return(
+            <div>
+                <SimpleCalculator/>
+            </div>
+        )
+    }
+}
+export default App;
