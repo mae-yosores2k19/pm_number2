7d070ce78e02177539a6a166b11bdb38a3691fec
mae-yosores2k19 <jessamaehortadoyosores@gmail.com>
2019-09-28 22:36:19 +0800

    Initial Commit

--- src/Component/App.js
+++ src/Component/App.js
@@ -1,15 +1,23 @@
+import React, { Component } from "react";
+import SimpleCalculator from "./SimpleCalculator";
+import "./Style.css";
 
-import React,{Component}from 'react';
-import SimpleCalculator from './SimpleCalculator'
 
-
-class App extends Component{
-    render(){
-        return(
-            <div>
-                <SimpleCalculator/>
-            </div>
-        )
-    }
+class App extends Component {
+  render() {
+    return (
+      <div>
+          <link
+          rel="stylesheet"
+          href="Style.css"
+        />
+        <link
+          rel="stylesheet"
+          href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css"
+        />
+        <SimpleCalculator />
+      </div>
+    );
+  }
 }
 export default App;
--- src/Component/SimpleCalculator.js
+++ src/Component/SimpleCalculator.js
@@ -1,63 +1,108 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class SimpleCalculator extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { num1: 0, num2: 0, answer: " "}
+  constructor(props) {
+    super(props);
+    this.state = { num1: 0, num2: 0, answer: " " };
+  }
+  handleFirstNum = e => {
+    this.setState({ num1: e.target.value });
+  };
+  handleSecondNum = e => {
+    this.setState({ num2: e.target.value });
+  };
+  handleOption = e => {
+    e.preventDefault();
+    if (e.target.value === "+") {
+      this.setState({ answer: Number(this.state.num1) + Number(this.state.num2 )});
     }
-    handleFirstNum = (e) => {
-        this.setState({ num1: e.target.value });
+    if (e.target.value === "-") {
+      this.setState({ answer: Number(this.state.num1) - Number(this.state.num2) });
     }
-    handleSecondNum = (e) => {
-        this.setState({ num2: e.target.value });
+    if (e.target.value === "*") {
+      this.setState({ answer: Number(this.state.num1) * Number(this.state.num2) });
     }
-    handleOption = (e) => {
-        e.preventDefault();
-        if(e.target.value === "+"){
-            this.setState({answer:this.state.num1 + this.state.num2})
-        }
-        if(e.target.value === "-"){
-            this.setState({answer:this.state.num1 - this.state.num2})
-        }
-        if(e.target.value === "*"){
-            this.setState({answer:this.state.num1 * this.state.num2})
-        }
-        if(e.target.value === "/"){
-            this.setState({answer:this.state.num1 / this.state.num2})
-        }
-        if(e.target.value === "^"){
-            this.setState({answer:this.state.num1 ** this.state.num2})
-        }
-        
-        
+    if (e.target.value === "/") {
+      this.setState({ answer: Number(this.state.num1) / Number(this.state.num2)});
     }
+    if (e.target.value === "^") {
+      this.setState({ answer: Number(this.state.num1) ** Number(this.state.num2)});
+    }
+  };
 
-    render() {
-        return (
-            <center>
+  render() {
+    return (
+      <div>
+        <center style={{ paddingTop: "20%" }}>
+          <div class="ui raised card " >
+            <div
+              class="content"
+              style={{
+                background:
+                  "linear-gradient(rgba(255,255,255,0.5),rgba(10,255,10,0.5)) , url(" +
+                  "https://previews.123rf.com/images/hermandesign2015/hermandesign20151801/hermandesign2015180100439/93381085-two-cute-pandas-in-a-bamboo-forest.jpg" +
+                  ") ",
+                backgroundSize: "cover",paddingBottom:"40%"
+              }}
+            >
+              <center>
+              <h1 style={{color:"red"}}> Basic Calculator </h1>
                 <div>
-                    {this.state.num1}
-                    {this.state.num2}
-                    <form>
-                        <input style={{ color: "black",fontSize:"20px", borderRadius: "10px", borderStyle: "inset" }} type="text" placeholder="Enter Number" onChange={(e) => this.handleFirstNum(e)}></input><span></span>
-                        <input style={{ color: "black",fontSize:"20px", borderRadius: "10px", borderStyle: "inset" }} type="text" placeholder="Enter Number" onChange={(e) => this.handleSecondNum(e)}></input>
-
-                    </form>
-
-                    <div style={{ color: "black", width: "50px",fontSize:"20px" }} >Result: {this.state.answer}</div><br></br>
-                    <div>
-                        <select onClick={(e)=>this.handleOption(e)}>
-                            <option value="+">Add</option>
-                            <option value="-">Subtract</option>
-                            <option value="*">Multiply</option>
-                            <option value="/">Divide</option>
-                            <option value="^">Exponent</option>
-                        </select>
+                  
+                  <form>
+                    <div class="ui  focus left icon input">
+                      <input
+                        style={{
+                          color: "black",
+                          borderStyle: "1px solid inset"
+                        }}
+                        type="number"
+                        placeholder="Enter Number"
+                        onChange={e => this.handleFirstNum(e)}
+                      ></input>
+                      <i aria-hidden="true" class="hashtag icon"></i>
                     </div>
-                </div>
-            </center>
+                    <span></span>
+                    <br/>
+                    <br/>
+                    <div class="ui  focus left icon input">
+                    <input
+                      style={{
+                      }}
+                      type="number"
+                      placeholder="Enter Number"
+                      onChange={e => this.handleSecondNum(e)}
+                    ></input>
+                     <i aria-hidden="true" class="hashtag icon"></i>
+                    </div>
+                  </form>
 
-        )
-    }
+                  <div  class="ui tiny teal message"
+                    style={{width:"78%"}}
+                  >
+                    Result: {this.state.answer}
+                  </div>
+                  <br></br>
+                  <div>
+
+                      
+                    <select onClick={e => this.handleOption(e)}>
+                      <option value="null">Select Operation: </option>
+                      <option value="+">Add</option>
+                      <option value="-">Subtract</option>
+                      <option value="*">Multiply</option>
+                      <option value="/">Divide</option>
+                      <option value="^">Exponent</option>
+                    </select>
+                  </div>
+                </div>
+              </center>
+            </div>
+          </div>
+        </center>
+        
+      </div>
+    );
+  }
 }
 export default SimpleCalculator;
